ea828487b5066af55104e5de1523ce4f8cc7584d
julia2524 <61957239+julia2524@users.noreply.github.com>
2024-01-28 20:51:11 +0900

    to do list

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    console.log(toDos);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const onDelete = (indexToDelete) => {
+    setToDos((currentArray) =>
+      currentArray.filter((_, index) => index !== indexToDelete)
+    );
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button id={index} onClick={() => onDelete(index)}>
+              {" "}
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
